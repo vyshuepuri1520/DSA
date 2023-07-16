e182cddb6af2a5f4781be4d44272298e6d974857
vyshu <saivyshu1520@gmail.com>
2023-07-16 18:10:03 +0530

    selectionsort

---
+++ Arrays/selectionsort.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class selectionsort {
+    public static int[] print(int arr[])
+	{
+		int n=arr.length;//6
+		for(int i=0;i<n;i++)
+		{
+            int temp=i;
+            for(int j=i;j<n;j++)
+            {
+                if(arr[temp]>arr[j])
+                {
+                    temp=j;
+                }
+            }
+            int sm=arr[temp];
+            arr[temp]=arr[i];
+            arr[i]=sm;
+	    }
+		return arr;
+   }
+	public static void main(String[] args)
+    {
+		int arr[]= {6,5,2,7,3,4};
+		int res[]=print(arr);
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(res[i]+" ");
+		}
+	}
+
+}
