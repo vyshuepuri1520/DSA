8af9fc89c9141aac66ac72a317df814fe4bd1081
vyshu <saivyshu1520@gmail.com>
2023-07-16 18:12:35 +0530

    linearsearch

---
+++ Arrays/linearseach.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class linearseach {
+    public static void main(String[] args)
+    {
+		int arr[] = {1,8,2,10,15,5};
+		int search = 10;
+		int temp = 0;
+		System.out.println(arr.length);
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i] == search)
+			{
+				temp = -1;
+				System.out.println("Find  "+arr[i]);
+			}
+		}
+		if(temp!=-1)	
+		System.out.println("Did not find the value");
+	}
+}
